98d34e234c88e3cc4c349ea7b1ad37b947aa70ba
Nico <nicolassciarrotta8@gmail.com>
2021-04-23 18:21:30 -0300

    Added new migrations

---
+++ backend/src/database/migrations/20210423205144_roles.js
@@ -0,0 +1,14 @@
+
+exports.up = async function(knex) {
+    await knex.schema.createTable('roles', table => {
+		table.bigIncrements('id');
+		table.string('key').notNullable();
+		table.string('description').notNullable();
+		table.boolean('isActive');
+		table.timestamp('createdAt').defaultTo(knex.fn.now());
+	});
+};
+
+exports.down = async function(knex) {
+    await knex.schema.dropTableIfExists('roles');
+};
