e95f860dd87aaf389f926aa71063b22fda5252f4
Lucas RK <lucasrk00@gmail.com>
2021-04-23 20:23:37 -0300

    comments to addressPOST

--- backend/src/routes/address/AddressPOST.js
+++ backend/src/routes/address/AddressPOST.js
@@ -7,12 +7,14 @@ module.exports = class AddressPOST extends Route {
 
 	async run(req, res) {
 		const { userId, street, number, floor, postalCode } = req.body;
+		// Check if body parameters are valid
 		if (!userId) return res.status(400).json({ message: 'userId is required!' });
 		if (!street) return res.status(400).json({ message: 'street is required!' });
 		if (!number) return res.status(400).json({ message: 'number is required!' });
 		if (!postalCode) return res.status(400).json({ message: 'postalCode is required!' });
 
 		try {
+			// Insert into database
 			await this.utils.addresses.createAddress({
 				userId,
 				street,
