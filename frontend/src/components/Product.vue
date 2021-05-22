ea90857b2d7727bbbddd4e8608300104c3cc95cb
Lucas RK <lucasrk00@gmail.com>
2021-05-21 23:58:53 -0300

    first product design

---
+++ frontend/src/components/Product.vue
@@ -0,0 +1,46 @@
+<template>
+	<div class="product-container">
+		<h2>{{ name }}</h2>
+		<p>{{ description }}</p>
+		<p>${{ price }}</p>
+		<b-button type="is-success" class="buy">Comprar</b-button>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		name: {
+			type: String,
+			required: true,
+		},
+		description: {
+			type: String,
+			required: true,
+		},
+		price: {
+			type: Number,
+			required: true,
+		},
+	}
+}
+</script>
+
+<style scoped lang="scss">
+.product-container {
+	display: flex;
+	flex-direction: column;
+	min-width: 10rem;
+	min-height: 10rem;
+	background-color: var(--blanco);
+	padding: .2rem;
+
+	h2 {
+		text-transform: capitalize;
+	}
+	.buy {
+		margin-top: auto;
+	}
+}
+
+</style>
--- frontend/src/views/home.vue
+++ frontend/src/views/home.vue
@@ -3,14 +3,27 @@
 		<h1>Colón Café Bar</h1>
 		{{ user }}
 		<hr>
-		{{ products }}
-		<router-link to="/login">Login</router-link>
+		<div class="container">
+			<div class="products-container">
+				<product v-for="(product, key) in products"
+					:key="key"
+					:name="product.name"
+					:price="product.price"
+					:description="product.description" />
+			</div>
+		</div>
+		<!--<router-link to="/login">Login</router-link>-->
 	</div>
 </template>
 
 <script>
+//import EditableText from '../../components/EditableText';
+import Product from '../components/Product.vue';
 export default {
 	name: 'Home',
+	components: {
+		Product,
+	},
 	data: () => ({
 		isLoading: true,
 	}),
@@ -40,6 +53,11 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped lang="scss">
+	.products-container {
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+		gap: 0.5rem;
+	}
 </style>
