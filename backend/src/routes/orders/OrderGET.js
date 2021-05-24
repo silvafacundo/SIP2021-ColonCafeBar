a007ce6907b80fbc68c3b478550b0a926bd34b1d
Facundo Silva <silva.facundo13@gmail.com>
2021-05-24 19:29:00 -0300

    improve order error

--- backend/src/routes/orders/OrderGET.js
+++ backend/src/routes/orders/OrderGET.js
@@ -10,6 +10,7 @@ module.exports = class OrderGET extends Route {
 			const { orderId } = req.query;
 
 			const order = await this.utils.orders.getOrder(orderId);
+			if (!order) return res.status(400).json({ message: 'Order doesn\'t exists' });
 			if (order.client.id !== user.id) return res.status(403).json({ message: 'Can\'t access to this resource' });
 
 			return res.json({ message: 'order successfully created', order });
