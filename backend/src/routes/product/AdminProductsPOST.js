d069ae11904e55534a9a87f52be390a0f436c7e0
Facundo Silva <silva.facundo13@gmail.com>
2021-05-25 19:05:05 -0300

    added admin products get

---
+++ backend/src/routes/product/AdminProductsPOST.js
@@ -0,0 +1,23 @@
+const Route = require('../../models/Route');
+
+module.exports = class ProductAllGET extends Route {
+	constructor() {
+		super('/admin/products', 'post');
+	}
+
+	async run(req, res) {
+		try {
+			//get all products
+			let { page = 1, perPage = 20, filters, orderBy } = req.body;
+
+			const products = await this.utils.products.getAllProducts({ page, perPage, filters: { ...filters }, orderBy });
+
+			return res.json({
+				message: 'Products successfully retrieved!',
+				products
+			});
+		} catch (error) {
+			return super.error(res, error);
+		}
+	}
+}
