5547fd5263ad1b07dfba3f6f258c0a15541704f4
Nico <nicolassciarrotta8@gmail.com>
2021-06-16 18:29:44 -0300

    products changes

--- frontend/src/views/Products.vue
+++ frontend/src/views/Products.vue
@@ -1,37 +1,81 @@
 <template>
-	<div class="container">
-		<b-field>
-			<b-input v-model="filters.query"
-				placeholder="Buscar..."
-				type="search"
-				icon="search"
-				:loading="isLoading || isSearching"
-				icon-clickable
-				@input="searchProduct"
-			/>
-		</b-field>
-		<div class="categories-container">
-			<div v-for="(category,keyc) in renderProducts"
-				:key="keyc"
-				class="category-container">
-				<h2> {{ category.category }} </h2>
-					<div class="product-container">
-						<product v-for="(product, key) in category.products"
-						:key="key"
-						:product="product"
-						@click="() => selectedProduct = product"
-						/>
+	<!-- <div class="container-principal">
+		<div class="sidebar-page">
+			<h2>Filtros</h2>
+			<b-field v-for="(category, key) in renderProducts"
+				:key="key">
+				<b-checkbox>{{ category.category }}</b-checkbox>
+			</b-field>
+		</div> -->
+		<div class="container">
+			<b-field>
+				<b-input v-model="filters.query"
+					placeholder="Buscar..."
+					type="search"
+					icon="search"
+					:loading="isLoading || isSearching"
+					icon-clickable
+					@input="searchProduct"
+				/>
+			</b-field>
+			<div class="categories-container">
+				<b-collapse class="card category-container" animation="slide" aria-id="contentIdForA11y3" v-for="(category,keyc) in renderProducts"
+					:key="keyc">
+					<template #trigger="props">
+						<div
+							class="card-header"
+							role="button"
+							aria-controls="contentIdForA11y3">
+							<h2 class="card-header-title">
+								<!-- <b-icon
+									pack="fas"
+									icon=""
+									size="is-small">
+								</b-icon> -->
+								{{ category.category }}
+							</h2>
+							<a class="card-header-icon">
+								<b-icon
+									:icon="props.open ? 'caret-down' : 'caret-up'">
+								</b-icon>
+							</a>
+						</div>
+					</template>
+					<div class="card-content">
+						<div class="content producto-container">
+							<product v-for="(product, key) in category.products"
+							:key="key"
+							:product="product"
+							@click="() => selectedProduct = product"
+							class="product" />
+						</div>
 					</div>
+				</b-collapse>
+				<h4 v-if="products.length <= 0" class="no-result">No se han encontrado resultados</h4>
 			</div>
-			<h4 v-if="products.length <= 0" class="no-result">No se han encontrado resultados</h4>
+			<!-- <div class="categories-container">
+				<div v-for="(category,keyc) in renderProducts"
+					:key="keyc"
+					class="category-container">
+					<h2> {{ category.category }} </h2>
+						<div class="product-container">
+							<product v-for="(product, key) in category.products"
+							:key="key"
+							:product="product"
+							@click="() => selectedProduct = product"
+							/>
+						</div>
+				</div>
+				<h4 v-if="products.length <= 0" class="no-result">No se han encontrado resultados</h4>
+			</div> -->
+			<b-modal :active="!!selectedProduct" @close="closeModal">
+				<product-selector v-if="selectedProduct"
+					:product="selectedProduct"
+					@addToCart="closeModal"
+					@cancel="closeModal" />
+			</b-modal>
 		</div>
-		<b-modal :active="!!selectedProduct" @close="closeModal">
-			<product-selector v-if="selectedProduct"
-				:product="selectedProduct"
-				@addToCart="closeModal"
-				@cancel="closeModal" />
-		</b-modal>
-	</div>
+	<!-- </div> -->
 </template>
 
 <script>
@@ -42,7 +86,7 @@ export default {
 	name: 'Products',
 	components: {
 		Product,
-		ProductSelector,
+		ProductSelector
 	},
 	data: () => ({
 		isLoading: true,
@@ -120,31 +164,49 @@ export default {
 </script>
 
 <style scoped lang="scss">
+// .container-principal{
+// 	display: flex;
+
+// 	.sidebar-page{
+// 		background-color: #fafafa;
+// 		height: 100vh;
+// 	}
 	.categories-container{
 
 		.category-container {
 			display: block;
-			margin-bottom: 2em;
+			margin-bottom: .5em;
 				h2{
-					color:#fafafa;
-					font-family: Holtzberg-Regular;
+					color:rgb(30,30,30);
+					font-family: BebasNeue-Regular, 'sans-serif';
 					font-size: 1.5em;
-					margin-bottom: 1em;
-					padding-bottom: .5em;
 					border-bottom:1px solid #fafafa;
 					user-select: none;
 				}
-				.product-container{
+				.producto-container{
 					display: grid;
-					// gap: 1rem;
 					grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+					grid-column-gap: 1em;
 					.no-result {
 						grid-column: 1/-1;
 						color: white;
 					}
+
+					.product{
+						display: block;
+						border:1px solid black;
+						box-shadow: 10px 10px 17px -11px rgba(0,0,0,0.55);
+						transition: .8s box-shadow;
+					}
+
+					.product:hover{
+						background-color:rgba(30,30,30, .1);
+						box-shadow: 10px 10px 17px -11px rgba(0,0,0,0.75);
+					}
 				}
 		}
 	}
+// }
 	@media (max-width: 900px){
 		div.container{
 			padding: 0;
